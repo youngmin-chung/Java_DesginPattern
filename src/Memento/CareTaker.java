7f81546473ef912ddfdd0851e24794ed2801bab9
Youngmin Chung <mintmin01@gmail.com>
2021-07-27 23:05:01 -0400

    Memento

---
+++ src/Memento/CareTaker.java
@@ -0,0 +1,20 @@
+package Memento;
+
+// Memento Design Pattern Tutorial
+
+import java.util.ArrayList;
+
+class CareTaker {
+
+    // Where all mementos are saved
+
+    ArrayList<Memento> savedArticles = new ArrayList<Memento>();
+
+    // Adds memento to the ArrayList
+
+    public void addMemento(Memento m) { savedArticles.add(m); }
+
+    // Gets the memento requested from the ArrayList
+
+    public Memento getMemento(int index) { return savedArticles.get(index); }
+}
