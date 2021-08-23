420db9fa837898a9d76e811a0ccddbe3bfc33dbe
Youngmin Chung <mintmin01@gmail.com>
2021-08-23 12:05:23 -0400

    Template Method

--- src/Observer/GrabStocks.java
+++ src/Observer/GrabStocks.java
@@ -1,6 +1,5 @@
 package Observer;
 
-
 /*
 * When use...
 * When we need many other objects to receive an update when another object changes
@@ -24,20 +23,6 @@ public class GrabStocks {
         stockGrabber.setIBMPrice(191.00);
         stockGrabber.setAPPLPrice(231.00);
         stockGrabber.setGOOGPrice(2451.00);
-
-//        StockObserver observer2 = new StockObserver(stockGrabber);
-//
-//        stockGrabber.setIBMPrice(192.00);
-//        stockGrabber.setAPPLPrice(232.00);
-//        stockGrabber.setGOOGPrice(2452.00);
-//
-//        // Delete one of the observers
-//
-//        // stockGrabber.unregister(observer2);
-//
-//        stockGrabber.setIBMPrice(193.00);
-//        stockGrabber.setAPPLPrice(233.00);
-//        stockGrabber.setGOOGPrice(2453.00);
 
         // Create 3 threads using the Runnable interface GetTheStock implements Runnable, so it doesn't waste
         // its one extendable class option
